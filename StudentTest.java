69709a199065a33fc4e9eb852970742bc309346e
jiyeony <bbologi94@gmail.com>
2015-11-02 19:52:10 +0900

    ee

--- StudentTest.java
+++ StudentTest.java
@@ -1,4 +1,6 @@
-﻿import java.util.Scanner;
+﻿package sowegong;
+
+import java.util.Scanner;
 import java.io.*;
 
 class Student implements Serializable {
@@ -7,15 +9,121 @@ class Student implements Serializable {
 	public String department;
 	public String phonNumber;
 }
- 
+
+class StudentClass {
+
+	Student[] st = new Student[100];
+
+	public static void add(Student[] st, Scanner scan, int i) {
+		if (i == 100)
+			System.out.println("Too many students records here");
+		else {
+			System.out.println("Enter students information");
+			st[i] = new Student();
+			scan.nextLine();
+			System.out.println("ID : ");
+			st[i].ID = scan.nextLine();
+			System.out.print("name:");
+			st[i].Sname = scan.nextLine();
+			System.out.print("department:");
+			st[i].department = scan.nextLine();
+			System.out.print("phone number:");
+			st[i].phonNumber = scan.nextLine();
+
+		}
+	}
+
+	public static void view(Student[] st, Scanner scan, int i) throws Exception {
+		for (int j = 0; j < i; j++) {
+			int k = j + 1;
+			System.out.println(k + ")\nID: " + st[j].ID + "\n" + "name: "
+					+ st[j].Sname + "\n" + "department: " + st[j].department
+					+ "\n" + "phone number: " + st[j].phonNumber);
+			System.out.println();
+		}
+	}
+
+	public static void update(Student[] st, Scanner scan, int i) {
+		Scanner scan1 = new Scanner(System.in);
+		System.out
+				.print("Enter the student's ID who you want to update phone number:");
+		String ID = scan1.nextLine();
+		for (int j = 0; j < i; j++) {
+			if (ID.equals(st[j].ID)) {
+				System.out.println("ID: " + st[j].ID + "\n" +
+
+				"name: " + st[j].Sname + "\n" + "department: "
+						+ st[j].department + "\n" + "phone number: " + st
+
+						[j].phonNumber);
+				System.out.println("Enter new phone number");
+				System.out.print("phone number:");
+				st[j].phonNumber = scan1.nextLine();
+				System.out.println("Update completed");
+				break;
+			} 
+			else 
+				System.out.println("There is no such ID");
+		}
+	}
+
+	public static void delete(Student[] st, Scanner scan, int i) {
+
+		Scanner scan2 = new Scanner(System.in);
+		System.out.print("Enter the student's ID who you want to delete:");
+		String ID1 = scan2.nextLine();
+		int t = -1;
+		for (int j = 0; j < i; j++) {
+			if (ID1.equals(st[j].ID)) {
+				System.out.println("ID: " + st[j].ID + "\n" + "name: "
+						+ st[j].Sname + "\n" + "department: "
+						+ st[j].department + "\n" + "phone number: "
+						+ st[j].phonNumber);
+				t = j;
+				break;
+			}
+		}
+		if (t != -1) {
+			for (; t < i; t++) {
+				st[t] = st[t + 1];
+			}
+			i = i - 1;
+			System.out.println("Delete completed");
+		} 
+		else 
+			System.out.println("There is no such ID");
+		
+	}
 	
-	public static void main(String[] args) throws Exception{
-		Student [] st = new Student[10];
-		int i=0;
+	public static void fileSave(Student [] st, int i) throws Exception{
+		ObjectOutputStream oos = new ObjectOutputStream (new FileOutputStream("tmp.txt"));
+		oos.writeInt(i);
+		for (int j = 0; j < i; j++)
+			oos.writeObject(st[j]);
+		oos.close();
+		System.out.println("All records are saved");
+	}
+	
+	public static void fileOpen(Student [] st, int i) throws Exception{
+		System.out.println("Records from file");
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("tmp.txt"));
+		i = ois.readInt();
+		for (int j = 0; j < i; j++) {
+			st[j] = (Student) ois.readObject();
+			System.out.println("ID: " + st[j].ID + "\n" + "name: "
+					+ st[j].Sname + "\n" + "department: "
+					+ st[j].department + "\n" + "phone number: "
+					+ st[j].phonNumber);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Student[] st = new Student[10];
+		int i = 0;
 		Scanner scan = new Scanner(System.in);
-		int menu=0;	
-		do{
-			do{
+		int menu = 0;
+		do {
+			do {
 				System.out.println("------------------------------");
 				System.out.println("1 add");
 				System.out.println("2 view");
@@ -27,95 +135,35 @@ class Student implements Serializable {
 				System.out.println("------------------------------");
 				System.out.print("menu");
 				menu = scan.nextInt();
-			}while(menu < 1 || menu > 7);
-			switch(menu){
-			case 1: //add
-				if(i == 100)
+			} while (menu < 1 || menu > 7);
+			switch (menu) {
+			case 1: // add
+				if (i == 100)
 					System.out.println("Too many students records here");
-				else{
-					System.out.println("Enter students information");
-					st[i] = new Student();
-					scan.nextLine();  
-					System.out.print("ID:");
-					st[i].ID = scan.nextLine();
-					System.out.print("name:");
-					st[i].Sname = scan.nextLine();
-					System.out.print("department:");
-					st[i].department = scan.nextLine();
-					System.out.print("phon number:");
-					st[i].phonNumber = scan.nextLine();
+				else {
+					add(st, scan, i);
 					i++;
 				}
 				break;
-			case 2: //view
-				for (int j = 0; j < i; j++) {
-					int k = j + 1;
-					System.out.println( k + ")\nID: " + st[j].ID +"\n"+ "name: " + st[j].Sname+ "\n" + "department: " + st[j].department + "\n" + "phone number: " + st[j].phonNumber);
-					System.out.println();
-				}
+			case 2: // view
+				view(st, scan, i);
 				break;
-			case 3: //update
-				Scanner scan1 = new Scanner(System.in);
-				System.out.print("Enter the student's ID who you want to update phone number:");
-				String ID = scan1.nextLine();
-			    for(int j = 0; j<i; j++){
-					if (ID.equals(st[j].ID)) {
-						System.out.println("ID: " + st[j].ID +"\n"+ "name: " + st[j].Sname+ "\n" + "department: " + st[j].department + "\n" + "phone number: " + st[j].phonNumber);
-						System.out.println("Enter new phone number");
-						System.out.print("phone number:");
-						st[j].phonNumber = scan1.nextLine();
-						System.out.println("Update completed");
-						break;
-					}
-					else{
-						System.out.println("There is no such ID");
-					}
-				}
-			    break;
-			case 4: //delete
-				Scanner scan2 = new Scanner(System.in);
-				System.out.print("Enter the student's ID who you want to delete:");
-				String ID1 = scan2.nextLine();
-			    int t = -1;
-			    for(int j = 0; j<i; j++){
-					if (ID1.equals(st[j].ID)) {
-						System.out.println("ID: " + st[j].ID +"\n"+ "name: " + st[j].Sname+ "\n" + "department: " + st[j].department + "\n" + "phone number: " + st[j].phonNumber);
-						t = j;
-						break;
-					}
-				}
-			    if(t != -1){
-			    	for(;t<i;t++){
-			    		st[t] = st[t+1];
-			    	}
-			    	i = i-1;
-			    	System.out.println("Delete completed");
-			    }
-			    else{
-			    	System.out.println("There is no such ID");
-			    }
-			    break;
-			case 5: //file save
-				ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("tmp.txt"));
-			    oos.writeInt(i);
-				for(int j = 0; j < i; j++)
-					oos.writeObject(mc[j]);
-				oos.close();
-				System.out.println("All records are saved");
+			case 3: // update
+				update(st, scan, i);
 				break;
-			case 6: //file open
-				System.out.println("Records from file");
-				ObjectInputStream ois = new ObjectInputStream(new FileInputStream("tmp.txt"));
-			    i = ois.readInt();
-				for(int j = 0; j<i; j++){
-					st[j] = (Student)ois.readObject();
-					System.out.println("ID: " + st[j].ID +"\n"+ "name: " + st[j].Sname+ "\n" + "department: " + st[j].department + "\n" + "phone number: " + st[j].phonNumber);
-				}
+			case 4: // delete
+				delete(st,scan,i);
 				break;
-			case 7: //quit
+			case 5: // file save
+				fileSave(st,i);
+				break;
+			case 6: // file open
+				fileOpen(st,i);
+				break;
+			case 7: // quit
 				System.out.println("Quit");
 			}
-		}while(menu != 7);
+		} while (menu != 7);
 	}
 
 }
